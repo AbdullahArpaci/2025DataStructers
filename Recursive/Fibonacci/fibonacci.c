ae7096fc9aff2888f957abe3c831439184bcb75c
Abdullah Arpacı <arpaciabdullah5151@gmail.com>
2025-12-17 02:36:20 +0300

    Calculate fibonacci with recursive

---
+++ Recursive/Fibonacci/fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int fib(int n);
+
+int main(){
+    int a = fib(5);
+    printf("%d",a);
+    return 0;
+}
+
+
+
+int fib(int n){
+    if(n == 0)
+        return 0;
+    if(n == 1)
+        return 1;
+    return fib(n-1) + fib(n-2);
+}
